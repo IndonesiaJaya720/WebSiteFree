d9ded33f460ef779bb10d39dce3089b606007d2d
IndonesiaJaya720 <72527640+IndonesiaJaya720@users.noreply.github.com>
2020-12-12 06:23:00 +0700

    Add files via upload

---
+++ WebsiteGratis.sh
@@ -0,0 +1,94 @@
+# ! / bin / bash
+
+red='\33[31;1m'
+yellow='\33[33;1m'
+blue='\33[0;36m'
+green='\33[0;32m'
+black='\33[30;1m'
+brown='\e[0;33m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightcyan='\e[1;36m'
+white='\e[1;37m'
+nc='\e[0m'
+sleep 3
+clear
+toilet -f big -F gay Website
+echo
+echo $blue
+echo = " Hi Say Mr_R5B "
+echo = " [1] Tools "
+echo = " [3] Follow Dulu Akun Saya "
+echo = " [4] https://github.com/IndonesiaJaya720/ "
+echo = " So much from me, thanks "
+sleep 3
+echo $black
+echo = " I Will Tell You How To Install Everything Yes "
+echo = " We Hug Materials : "
+echo = " Di Atas Ya Baca "
+echo $red
+echo " =========================================="
+echo " =========================================="
+echo " ||    Atthur = Mr_R5B                   ||"
+echo " ||    WhatsApp = +628......             ||"
+echo " ||    Hobi = Website Hacker             ||"
+echo " ||    Message = Link = Tools = Termux   ||"
+echo " =========================================="
+echo " =========================================="
+echo
+echo $brown
+echo " { https://github.com/ } "
+echo " { http://defaceindonesia.6te.net/MR_R5B } "
+echo " { http://defaceindonesia.6te.net/ } "
+echo " { http://anonymousindonesia.6te.net/ } "
+echo
+echo $cyan
+echo [•] Installation Starts in 3 Seconds
+sleep 3
+echo [•] Install Data Yes First
+echo [•] Installation Starts in 3 Seconds
+sleep 3
+echo $blue
+echo " [%%%%%%] "
+sleep 1
+echo " [%%%%%%%%%%] "
+sleep 2
+echo " [%%%%%%%%%%%] "
+sleep 3
+echo " [%%%%%%%%%%%%] "
+sleep 4
+echo " [%%%%%%%%%%%%%] "
+sleep 5
+echo " [%%%%%%%%%%%%%%] "
+sleep 6
+echo " [%%%%%%%%%%%%%%%] "
+sleep 7
+echo " [%%%%%%%%%%%%%%%%] "
+sleep 8
+echo " [%%%%%%%%%%%%%%%%%] "
+sleep 9
+echo " [%%%%%%%%%%%%%%%%%%%] "
+sleep 10
+echo " [√] Thank you for waiting "
+echo " [√] Time We Read Success "
+echo " [√] "
+echo $yellow
+echo " [ ~ [ ~ [ ~ [ ~ [ [ ~ [ ~ [ ~ [ ~ [ ~ ] "
+echo " ] ~ [ ~ ] ~ [ ~ ] [ ~ ] ~ [ ~ [ ~ [ ~ ] "
+echo "            [ 1 ] Website Gratis         "
+echo "            [ 2 ] Exit                   "
+echo " [ ~ [ ~ [ ~ [ ~ [ [ ~ [ ~ [ ~ [ ~ [ ~ ] "
+echo " ] ~ [ ~ ] ~ [ ~ ] [ ~ ] ~ [ ~ [ ~ [ ~ ] "
+read -p "masukan pilihan anda di sini :" Bro
+if [ $Bos = 1 ] || [ $Bos = 1 ]
+then
+         echo == [ ........]
+         echo == [ Di Bawah Ya ]
+         echo "    https://freewha.com/     "
+exit
+fi
+
+if [ $Bos = 2 ] || [ $Bos = 2 ]
+then
+exit
+fi
